6a8754c766f25804ba0dffd209c8d702847bb778
thyra <148661633+thyraxxxx@users.noreply.github.com>
2025-11-25 14:19:51 -0500

    Use to drop tables (just in case)

---
+++ drop_tables_schema.sql
@@ -0,0 +1,7 @@
+USE restaurant_api;
+
+DROP TABLE IF EXISTS reservations;
+DROP TABLE IF EXISTS menu_items;
+DROP TABLE IF EXISTS `tables`;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS restaurants;
